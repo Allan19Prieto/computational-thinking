67395edac21b95e1f3e3b02c49e9ac07ff1f14b0
Allan Prieto <allanbanilla@gmail.com>
2023-02-02 22:13:49 -0600

    Comments

---
+++ Python/initials.py
@@ -0,0 +1,9 @@
+# This is a comment and my name
+
+print('  A    L     L      A    N      N')
+print(' A A   L     L     A A   N N    N')
+print('A   A  L     L    A   A  N  N   N')
+print('AAAAA  L     L    AAAAA  N   N  N')
+print('A   A  L     L    A   A  N    N N')
+print('A   A  L     L    A   A  N     NN')
+print('A   A  LLLLL LLLL A   A  N      N')
