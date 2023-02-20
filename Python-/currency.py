0e9d1d585607588340ca94ed8f66bc2d52972164
Allan Prieto <allanbanilla@gmail.com>
2023-02-20 00:13:03 -0600

    Currency

---
+++ Python-/currency.py
@@ -0,0 +1,8 @@
+
+yuan = int(input('What do you have left in yuan? '))
+yen = int(input('what do you have left in yen? '))
+won = int(input('what do you have left in won? '))
+
+total = yuan * 0.15 + yen * 0.0077 + won * 0.00080
+
+print(total);
